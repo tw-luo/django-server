54b100ca1b80beb12aeb1f9fec980f1edf7375b7
tw-luo <45718910+tw-luo@users.noreply.github.com>
2023-03-23 06:01:40 +0000

    Initial commit

---
+++ hello_world/core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(
+        request,
+        "index.html",
+        {
+            "title": "Django example",
+        },
+    )
