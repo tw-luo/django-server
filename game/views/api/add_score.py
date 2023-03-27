5893d9dfba3ddc1a67ad7ebec2fd6bd469265c33
tw-luo <45718910+tw-luo@users.noreply.github.com>
2023-03-27 09:10:29 +0000

    修复了api的bug

--- game/views/api/add_score.py
+++ game/views/api/add_score.py
@@ -5,8 +5,8 @@ from django.http import JsonResponse
 #register
 def add_score(request):
     if request.user.is_authenticated:
-        score_type=request.test_type      
-        test_score=request.score
+        score_type=request.GET.get('test_type')    
+        test_score=request.GET.get('score')
         new_score=Score(user=request.user,test_type=score_type,score=test_score,test_date=timezone.now())
         new_score.save()
     else:
