c6ce77c06e2cef74786edd2c8b1985a5a5bf4b2f
tw-luo <45718910+tw-luo@users.noreply.github.com>
2023-03-27 12:00:04 +0000

    获取最新的前端界面

--- update.sh
+++ update.sh
@@ -8,4 +8,7 @@ cp build/index.html ../hello_world/templates/index.html
 cp -r build/static/js ../hello_world/static/js
 cp -r build/static/css ../hello_world/static/css
 
+cd ..
+git add .
+git commit -m "获取最新的前端界面"
 git push
